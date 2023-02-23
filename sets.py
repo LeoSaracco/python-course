fd96bbcfab5afcdddda0c4527e3c714ac583aa82
Leandro Saracco <leosaracco@gmail.com>
2023-02-23 16:31:27 -0300

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,12 @@
+# Son para definir una colección que no tiene índices
+colores = {"Rojo", "Negro", "Violeta"}
+print(colores)
+# Si existe
+print("Red" in colores)
+# Agregar colores
+colores.add("Esmeralda")
+# Eliminar un color
+colores.remove("Negro")
+# Limpiar lista
+# colores.clear()
+print(colores)
---
+++ tuples.py
@@ -0,0 +1,10 @@
+x = (1,2,3)
+# Índice 
+print(x[2])
+
+# Tuplas con diccionarios
+locations = {
+    (1.85,"80KG"): "Leandro",
+    (1.65,"60KG"): "Juana"
+}
+print(locations)
