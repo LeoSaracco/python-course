351a410deb9c48b667c72f07d45e72d547b8eaf8
Leandro Saracco <leosaracco@gmail.com>
2023-03-11 16:52:27 -0300

    Add files via upload

--- scheduler/jobPython.py
+++ scheduler/jobPython.py
@@ -1,6 +1,7 @@
 # To run this proyect you have to excecute "pip install schedule" previously run the app
 import schedule
 import time
+import requests
 
 def job():
     print("Reading job")
@@ -8,11 +9,20 @@ def job():
 def jobTimeSet():
     print("Reading job at " + time.asctime(time.localtime()))
 
-# To ten second
-schedule.every(10).seconds.do(job)
+def getDolarPrice():
+    url = "https://api.bluelytics.com.ar/v2/latest"
+    page = requests.get(url)
+    data = page.json()
+    print("Valor dolar:\n", data["blue"])
+
+# To thirty second
+schedule.every(30).seconds.do(job)
 
 # To specific time
-schedule.every().day.at("16:40").do(jobTimeSet)
+schedule.every().day.at("16:50").do(jobTimeSet)
+
+# To specific time calling API
+schedule.every().day.at("16:51").do(getDolarPrice)
 
 while True:
     schedule.run_pending()
