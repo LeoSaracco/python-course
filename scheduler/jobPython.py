fb3709622fad5a625a269b01d9ffb40a9e46370c
Leandro Saracco <leosaracco@gmail.com>
2023-03-11 16:29:39 -0300

    Add files via upload

--- scheduler/jobPython.py
+++ scheduler/jobPython.py
@@ -1,9 +1,11 @@
+# To run this proyect you have to excecute "pip install schedule" previously run the app
 import schedule
 import time
 
 def job():
     print("Reading job")
 
+# To every second
 schedule.every(1).seconds.do(job)
 
 while True:
